9a1569e783d419f224feb157d697d895cae1c8a0
rcor23 <rcor23>
2023-10-15 17:33:43 -0300

    sacar

--- src/main/Investimento.java
+++ src/main/Investimento.java
@@ -6,6 +6,8 @@ public class Investimento extends Conta {
     private static final double IMPOSTO = 0.15; 
     private static final double TAXA = 0.015; 
 
+    private double rendimento = 0;
+
     public Investimento(Cliente cliente) {
         super(cliente);
     }
@@ -14,20 +16,44 @@ public class Investimento extends Conta {
     public void atualizarSaldo() {
         double saldoAtual = getSaldo();
 
-
         double variacaoDiaria = (new Random().nextDouble() - 0.5) / 10.0;
-        
-    
-        double rendimentoDiario = saldoAtual * variacaoDiaria;
-        
-    
-        double imposto = rendimentoDiario * IMPOSTO;
-        
-    
-        double taxa = rendimentoDiario * TAXA;
-        
 
-        saldoAtual += rendimentoDiario - imposto - taxa;
+        double saldoAnterior = saldoAtual; // Saldo anterior antes da variação
+
+        // Calcule o rendimento diário como a diferença entre o saldo atual após a variação e o saldo anterior
+        double rendimentoDiario = (saldoAtual * variacaoDiaria) - saldoAnterior;
+
+        this.rendimento += rendimentoDiario;
+
+        saldoAtual += rendimentoDiario;
         setSaldo(saldoAtual);
     }
 }
+
+
+ public void sacar(double valor) {
+        double saldoAtual = getSaldo();
+
+        if (valor <= 0) {
+            System.out.println("O valor do saque deve ser positivo.");
+        } else {
+            double rendimento = this.rendimento;
+            double imposto = rendimento * IMPOSTO;
+
+            if (saldoAtual >= valor + imposto) {
+                saldoAtual -= (valor + imposto);
+                setSaldo(saldoAtual);
+                System.out.println("Saque de R$" + valor + " realizado com sucesso.");
+            } else {
+                System.out.println("Saldo insuficiente para realizar o saque após o desconto de imposto.");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
